8b67a5a1d90d025bfa202b9e56e93ca74b4967ae
cjcobra <cjcobra2@gmail.com>
2019-02-27 22:24:01 -0500

    Base

---
+++ KnockyBalls02/Assets/collisionDetection.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class collisionDetection : MonoBehaviour
+{
+
+    private float score = 0;
+    public Text ScoreText;
+    public Text YouWon;
+
+    void OnCollisionEnter(Collision col)
+    {
+     //   Debug.Log (gameObject.name + " has collided with " + col.gameObject.name);
+
+        if (col.gameObject.name == "Brick")
+        {
+            score++;
+            SetScoreText();
+
+            Debug.Log("Score: " + score);
+            Destroy(col.gameObject);
+        }
+    }
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        YouWon.text = "";
+
+        Debug.Log("Score: " + score);
+        SetScoreText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetScoreText()
+    {
+       // score++;
+
+        ScoreText.text = "Score: " + score.ToString();
+        if (score == 12)
+        {
+            YouWon.text = "YOU WON!";
+        }
+
+    }
+
+}
---
+++ KnockyBalls02/Assets/playerScript02.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class playerScript02 : MonoBehaviour
+{
+
+    public Rigidbody rocketPrefab;
+    public Transform barrelEnd;
+
+   // public GameObject projectile;
+   public float bulletSpeed;
+
+    /*
+    public float moveForce = 0f;
+    private Rigidbody rbody;
+    public GameObject bullet;
+    public Transform gun;
+    public float shootRate = 0f;
+    public float shootForce = 0f;
+    private float shootRateTimeStamp = 0f;
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+      //  rbody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody rocketInstance;
+            rocketInstance = Instantiate(rocketPrefab, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
+            rocketInstance.AddForce(barrelEnd.forward * bulletSpeed);
+
+            Debug.Log("playerScript02");
+        }
+
+
+
+
+        /*
+        if (Input.touchCount >= 0)
+        {
+
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+
+                Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                Vector3 dir = touchPos - (new Vector3(transform.position.x, transform.position.y));
+                dir.Normalize();
+                GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+                bullet.GetComponent<Rigidbody2D>().velocity = dir * bulletSpeed;
+            }
+        }
+        */
+
+        /*
+        float h = Input.GetAxisRaw("Horizontal") * moveForce;
+        float v = Input.GetAxisRaw("Vertical") * moveForce;
+
+        rbody.velocity = new Vector3(h, v, 0);
+
+        if(Input.GetMouseButtonDown(0))   //Input.GetKey(KeyCode.Space)
+        {
+            if(Time.time > shootRateTimeStamp)
+            {
+                GameObject go = (GameObject)Instantiate(bullet, gun.position, gun.rotation);
+                go.GetComponent<Rigidbody>().AddForce(gun.forward * shootForce);
+                shootRateTimeStamp = Time.time + shootRate;
+
+            }
+                       
+        }
+        */
+
+
+
+
+
+    }
+}
