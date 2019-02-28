cace3f5203b54855bf9906be17a3902990871372
cjcobra <cjcobra2@gmail.com>
2019-02-27 21:06:47 -0500

    Base Upload
    First Upload

---
+++ Assets/platformMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformMove : MonoBehaviour
+{
+
+    public float speed = 2.5f;
+  //  float myTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+  //      myTimer = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -speed * Time.deltaTime, 0);
+
+    //    transform.position = new Vector3 (Mathf.PingPong(myTimer = speed, 5), transform.position.y, transform.position.z);
+
+    }
+}
